81077bbae32e2bdd423fc16ca93c68c189508dde
sudosubin <sudosubin@gmail.com>
2025-03-30 20:24:35 +0900

    feat: update nix flake files

--- flake.nix
+++ flake.nix
@@ -2,26 +2,24 @@
   description = "sudosubin/pirogramming-19th-deployment";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = import nixpkgs { inherit system; config.allowUnsupportedSystem = true; };
+  outputs = { self, nixpkgs }:
+    let
+      inherit (nixpkgs.lib) genAttrs platforms;
+      forAllSystems = f: genAttrs platforms.unix (system: f (import nixpkgs { inherit system; }));
 
-        in
-        {
-          devShell = pkgs.mkShell rec {
-            venvDir = "./.venv";
-
-            buildInputs = with pkgs; [
-              python311
-              python311Packages.venvShellHook
-            ];
-          };
-        }
-      );
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          venvDir = "./.venv";
+          buildInputs = with pkgs; [
+            python311
+            python311Packages.venvShellHook
+          ];
+        };
+      });
+    };
 }
